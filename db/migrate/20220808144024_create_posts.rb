3159135481b75d05d70abb03f87c259ff11ebf05
Aimal Amiri <meaimal@hotmail.com>
2022-08-08 19:19:43 +0430

    Fix relationship in post migration

--- db/migrate/20220808144024_create_posts.rb
+++ db/migrate/20220808144024_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.belongs_to user, foreign_key: true, index: true
+      t.belongs_to :user, foreign_key: true, index: true
       t.string :title
       t.text :text
 
