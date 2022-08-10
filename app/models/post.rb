8d04a39855f834bbb30dfadb5449a47362d6096c
Aimal Amiri <meaimal@hotmail.com>
2022-08-10 13:14:19 +0430

    Add last_five_comments method to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user, counter_cache: true
   has_many :comments
   has_many :likes
+
+  def last_five_comments
+    comments.order(id: :DESC).limit(5)
+  end
 end
