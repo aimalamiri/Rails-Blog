fda9d0d1c3c57ea372a88f3f434b4279f6fac43a
Aimal Amiri <meaimal@hotmail.com>
2022-08-16 16:47:32 +0430

    Add tests for posts/show action

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe 'PostsController', type: :request do
     get '/users/1/posts'
     expect(response.body).to include 'posts'
   end
+
+  it 'returns status 200 Ok for /users/1/posts/1' do
+    get '/users/1/posts/1'
+    expect(response).to have_http_status :success
+  end
+
+  it 'returns posts/show view for posts/show action' do
+    get '/users/1/posts/1'
+    expect(response).to render_template :show
+  end
+
+  it 'returns posts/show view that includes post keyword' do
+    get '/users/1/posts/1'
+    expect(response.body).to include 'Post'
+  end
 end
