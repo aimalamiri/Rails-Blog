793fcd119887e0a51fc5a8c7e8169185ac11119d
Aimal Amiri <meaimal@hotmail.com>
2022-08-23 13:51:01 +0430

    Add test for users image

--- spec/capybara/users_spec.rb
+++ spec/capybara/users_spec.rb
@@ -4,12 +4,18 @@ RSpec.describe 'Users Page', type: :system do
   before :all do
     @users = User.all
   end
+
   describe 'index page' do
     it 'Show the user name of all users' do
       visit users_path
       @users.each do |user|
         expect(page).to have_content(user.name)
       end
+    end
+
+    it 'Should load the image of each user' do
+      visit users_path
+      expect(page).to have_selector('img')
     end
 
     it 'Show the posts_count for all users' do
