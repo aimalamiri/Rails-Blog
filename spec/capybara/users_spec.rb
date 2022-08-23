44707357328ad44a41a1d5c1600d22c744365d56
Aimal Amiri <meaimal@hotmail.com>
2022-08-23 13:57:55 +0430

    Add tests for users links

--- spec/capybara/users_spec.rb
+++ spec/capybara/users_spec.rb
@@ -24,5 +24,14 @@ RSpec.describe 'Users Page', type: :system do
         expect(page).to have_content("#{user.posts_count} Posts")
       end
     end
+
+    it 'Should redirect to the users show page when the user clicks on the link' do
+      visit users_path
+      @users.each do |user|
+        click_link(user.name)
+        expect(page).to have_content(user.bio)
+        visit users_path
+      end
+    end
   end
 end
