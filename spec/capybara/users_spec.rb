932a17186495cd464b1ca9d38c6007f3720980ab
Aimal Amiri <meaimal@hotmail.com>
2022-08-23 13:23:55 +0430

    Add test for all users names are visible in the page

--- spec/capybara/users_spec.rb
+++ spec/capybara/users_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'Users Page', type: :system do
+  before :all do
+    @users = User.all
+  end
   describe 'index page' do
-    it 'Show all users page in the title' do
+    it 'Show the user name of all users' do
       visit users_path
-      expect(page).to have_content('All users page')
+      @users.each do |user|
+        expect(page).to have_content(user.name)
+      end
     end
   end
 end
