695269733812824c03e5ca672795e258aa993319
Aimal Amiri <meaimal@hotmail.com>
2022-08-23 13:25:25 +0430

    Add test for users posts count in the users index page

--- spec/capybara/users_spec.rb
+++ spec/capybara/users_spec.rb
@@ -11,5 +11,12 @@ RSpec.describe 'Users Page', type: :system do
         expect(page).to have_content(user.name)
       end
     end
+
+    it 'Show the posts_count for all users' do
+      visit users_path
+      @users.each do |user|
+        expect(page).to have_content("#{user.posts_count} Posts")
+      end
+    end
   end
 end
