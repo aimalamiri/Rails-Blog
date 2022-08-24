4f1b73d031bcb889cf70605f02572aa857749da6
Aimal Amiri <meaimal@hotmail.com>
2022-08-24 21:11:27 +0430

    Add logout link to navbar, and fix devise bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  # devise_scope :user do
+  #   get "users", to: "devise/sessions#new"
+  # end
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
