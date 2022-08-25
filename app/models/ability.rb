7305c3809199ef2c10ccf7469703393e0319acc1
Aimal Amiri <meaimal@hotmail.com>
2022-08-25 11:22:34 +0430

    Fix linter errors

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,14 +1,14 @@
-# frozen_string_literal: true
-​
 class Ability
   include CanCan::Ability
-​
+
   def initialize(user)
     return unless user.present?
+
     can :delete, Comment, user: user
     can :delete, Post, user: user
-​
+
     return unless user.admin?
+
     can :delete, Post
     can :delete, Comment
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,6 @@ class User < ApplicationRecord
   end
 
   def admin?
-    role === 'admin'
+    role == 'admin'
   end
 end
